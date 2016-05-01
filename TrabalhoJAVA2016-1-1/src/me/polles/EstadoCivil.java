9b3307a664bcf9c9ec1499114036c2e755733276
João Paulo Polles <joaopaulopolles@gmail.com>
2016-05-01 16:16:30 -0300

    Criado o método do enum EstadoCivil e feita uma modificação no nome da variável que recebe a conexão em DaoI

--- TrabalhoJAVA2016-1-1/src/me/polles/EstadoCivil.java
+++ TrabalhoJAVA2016-1-1/src/me/polles/EstadoCivil.java
@@ -9,6 +9,11 @@ public enum EstadoCivil {
 	private String estadoCivil;
 	private final int id;
 	
+	private EstadoCivil(String estadoCivil, int id){
+		this.estadoCivil = estadoCivil;
+		this.id = id;
+	}
+	
 	public String getEstadoCivil(){
 		return estadoCivil;
 	}
