7bd8d8e5c1733e08e463689132c74d3852e68de0
João Paulo Polles <joaopaulopolles@gmail.com>
2016-05-01 15:58:15 -0300

    Criados os getters and setters da Classe de Cliente e também setado qual a tabela e campos dela no banco de dados...

--- TrabalhoJAVA2016-1-1/src/me/polles/Cliente.java
+++ TrabalhoJAVA2016-1-1/src/me/polles/Cliente.java
@@ -1,9 +1,64 @@
 package me.polles;
 
+
+@Tabela("CLIENTE")
 public class Cliente {
+	
+	@Coluna(pk=true)
 	private int id;
+	
+	@Coluna(nome="NOME")
 	private String nome;
+	
+	@Coluna(nome="ENDERECO")
 	private String endereco;
+	
+	@Coluna(nome="TELEFONE")
 	private String telefone;
+
+	@Coluna(nome="CLESTADOCIVIL")
+	private EstadoCivil estadoCivil;
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
 
 }
